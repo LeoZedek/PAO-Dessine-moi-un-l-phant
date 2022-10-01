260b1565b35b43922a964700b8db77d9cb6dfe01
Solène Perret <solene.perret@insa-rouen.fr>
2022-10-01 16:50:42 +0200

    Modif syntaxe pour améliorer lsibilité

--- point.py
+++ point.py
@@ -1,53 +1,44 @@
 #!/usr/bin/env python3
 """Module proposant la classe Point2D"""
 
-from numpy import angle , abs
 from math import hypot
+from numpy import angle , abs
 
 class Point2D:
-	def __init__(self, x: float, y: float):
-		self._x = x
-		self._y = y
-		self._module  = abs(complex(self))
-		self._argument = angle(complex(self))
+    "Classe représentant un point 2D" 
+    def __init__(self, x: float, y: float):
+        "Méthode permettant d'initialiser un Point2D"
+        self._x = x
+        self._y = y
+        self._module  = abs(complex(self))
+        self._argument = angle(complex(self)) 
 
-	@property
-	def x(self)->float:
-		return self._x
+    @property
+    def x(self)->float:
+        return self._x
 
-	@property
-	def y(self)->float:
-		return self._y
+    @property
+    def y(self)->float:
+        return self._y
+    @property 
+    def module(self)->float:
+        return self._module
 
-	@property 
-	def module(self)->float:
-		return self._module
+    @property
+    def argument(self)->float:
+        return self._argument
 
-	@property
-	def argument(self)->float:
-		return self._argument
+    def __complex__(self):
+        return complex(self.x,self.y)
 
-	def __complex__(self):
-		return complex(self.x,self.y)
+    def distance(self, point2):
+        if self.x == point2.getX():
+            return abs(self.y() - point2.getY())
+        if self.y() == point2.y():
+            return abs(self.x() - point2.x())
+        return hypot(self.x() - point2.x(), self.y() - point2.y())
 
-	def distance(self, point2):
-		if self.getX() == point2.getX():
-			return abs(self.getY() - point2.getY())
-
-		if self.getY() == point2.getY():
-			return abs(self.getX() - point2.getX())
-
-		return hypot(self.getX() - point2.getX(), self.getY() - point2.getY())
-    
-	def linearEquation(self, point2):
-		coeffA = (point2.getY() - self.getY()) / (point2.getX() - self.getX())
-		coeffB = self.getY() - coeffA * self.getX()
-
-		return coeffA, coeffB
-
-            
-
-
-
-
-    
+    def linear_equation(self, point2):
+        coeff_a = (point2.y() - self.y()) / (point2.x() - self.x())
+        coeff_b = self.y() - coeff_a * self.x()
+        return coeff_a, coeff_b
