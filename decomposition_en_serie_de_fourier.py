cc86d8fe1ecb447fde28fe170c6f4479c6b2ede9
Solène Perret <solene.perret@insa-rouen.fr>
2022-10-06 17:31:09 +0200

    Rangement de la liste des coefficients de la decomposition en série de Fourier

--- decomposition_en_serie_de_fourier.py
+++ decomposition_en_serie_de_fourier.py
@@ -12,6 +12,18 @@ def calcul_coefficient(points_complexes,indice)->complex:
     somme = np.trapz(points_complexes * np.exp(-1j* indice *temps * 2 * np.pi / periode),dx=pas)
     return somme/periode
 
+
+def rangement(coefficients,nb_cercle):
+    nouvelle_liste = []
+    middle_ind =len(coefficients) // 2
+    for i in range(1, middle_ind+1):
+        nouvelle_liste.append(coefficients[middle_ind - i]) 
+        nouvelle_liste.append(coefficients[middle_ind + i]) 
+    if nb_cercle % 2==1:
+        nouvelle_liste.pop()
+    print(nouvelle_liste)
+    return nouvelle_liste
+
 def decompositions_en_serie_de_fourier(points_complexes,nb_cercle)->list[complex]:
     """ Fonction retournant l'ensemble des coefficients de l a décomposition en série de Fourier """
     borne = nb_cercle // 2
@@ -19,7 +31,9 @@ def decompositions_en_serie_de_fourier(points_complexes,nb_cercle)->list[complex
         coefficients = [calcul_coefficient(points_complexes,indice_coefficients)\
         for indice_coefficients in range(-borne,borne+1) ]
     else:
+        borne = borne + 1
         coefficients = [calcul_coefficient(points_complexes,indice_coefficients)\
-        for indice_coefficients in range(-borne,borne+2) ]
-
+        for indice_coefficients in range(-borne,borne+1)]
+    coefficients=rangement(coefficients,nb_cercle)
     return coefficients
+
