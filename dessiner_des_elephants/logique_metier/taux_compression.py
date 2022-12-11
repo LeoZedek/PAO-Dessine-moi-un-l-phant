2426cd0a6dc34bff2a82b3ce18768b316a619058
Alexis IMBERT <Alexis IMBERT>
2022-12-11 12:34:17 +0100

    ajout d'une fonction calculant le taux de compression

---
+++ dessiner_des_elephants/logique_metier/taux_compression.py
@@ -0,0 +1,17 @@
+"""
+Fichier regroupant les taux de compressions calculé
+@author:Alexis IMBERT
+"""
+
+
+def taux_de_compression(nb_points: int, nb_cercle: int) -> float:
+    """
+    Calcul le taux de compression en pourcentage
+    # Parameter
+     - nb_points : le nombre de points gardé pour le dessin
+     - nb_cercle : le nombre de cercle stocké afin de reproduire le dessin
+
+     # Return :
+     float : le taux de compression en pourcentage
+    """
+    return round((nb_points-nb_cercle) /(nb_points),4)*100
