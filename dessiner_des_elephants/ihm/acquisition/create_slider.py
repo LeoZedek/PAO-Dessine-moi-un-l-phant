d72f8b9635a5b5b0558f17f95266f5ab601dd54e
Leo <leo.zedek@insa-rouen.fr>
2022-12-19 18:11:35 +0100

    create_slider modulation and constant create for slider min and max value and step

---
+++ dessiner_des_elephants/ihm/acquisition/create_slider.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+"""Module proposant les fonctions qui créer les sliders"""
+import pygame as pg
+
+from pygame_widgets.slider import Slider
+
+from dessiner_des_elephants.ihm.affichage.constructed_rectangles import ConstructedRectangles
+from dessiner_des_elephants.ihm.affichage.draw_elephant_utils import SLIDER_COLOR,\
+                                                                     SLIDER_HANDLE_COLOR, \
+                                                                     MIN_CIRCLE_SLIDER_VALUE,\
+                                                                     MAX_CIRCLE_SLIDER_VALUE,\
+                                                                     CIRCLE_SLIDER_STEP,\
+                                                                     MIN_POINTS_SLIDER_VALUE,\
+                                                                     MAX_POINTS_SLIDER_VALUE,\
+                                                                     POINTS_SLIDER_STEP,\
+                                                                     PROPORTION_PARAMETERS_BUTTON
+from dessiner_des_elephants.logique_metier.point import Point2D
+
+def create_sampling_slider(screen : pg.Surface,\
+                            constructed_rectangle : ConstructedRectangles,\
+                            points : list[Point2D]):
+
+    """
+      Créer le slider pour le nombre de points
+
+      screen : Surface sur laquel l'utilisateur va utiliser le slider
+      constructed_rectangle : l'objet constructed_rectangle qui contient tous les rectangles
+      points : list de Point2D, utile pour changer le min ou le max du slider si besoin
+
+      return : le slider pour choisir le nombre de points échantillonés
+    """
+
+    original_drawing_rectangle = constructed_rectangle.original_drawing_rectangle
+    sampling_box = constructed_rectangle.sampling_box
+    number_circle_box = constructed_rectangle.number_circle_box
+
+    width_slider = (screen.get_size()[0] - original_drawing_rectangle.width) \
+                   * PROPORTION_PARAMETERS_BUTTON
+    height_slider = number_circle_box.top - sampling_box.top \
+                    - sampling_box.height - constructed_rectangle.box_padding_ordinate
+
+    left_slider_sampling = original_drawing_rectangle.width * 1.05
+    top_slider_sampling = sampling_box.top + sampling_box.height \
+                          + (constructed_rectangle.box_padding_ordinate // 2)
+
+    min_sampling = min(len(points), MIN_POINTS_SLIDER_VALUE)
+    max_sampling = min(len(points), MAX_POINTS_SLIDER_VALUE)
+
+    slider_sampling = Slider(screen, left_slider_sampling, top_slider_sampling, \
+                             width_slider, height_slider, \
+                             min = min_sampling, max = max_sampling, \
+                             step = POINTS_SLIDER_STEP, \
+                             handleColour = SLIDER_HANDLE_COLOR, colour = SLIDER_COLOR)
+    return slider_sampling
+
+def create_number_circle_slider(screen : pg.Surface,\
+                                 constructed_rectangle : ConstructedRectangles):
+
+    """
+      Créer le slider pour le nombre de cercle
+
+      screen : Surface sur laquel l'utilisateur va utiliser le slider
+      constructed_rectangle : l'objet constructed_rectangle qui contient toutes les boites
+
+      return : le slider pour choisir le nombre de cercle
+    """
+
+    original_drawing_rectangle = constructed_rectangle.original_drawing_rectangle
+    sampling_box = constructed_rectangle.sampling_box
+    number_circle_box = constructed_rectangle.number_circle_box
+
+    width_slider = (screen.get_size()[0] - original_drawing_rectangle.width) \
+                   * PROPORTION_PARAMETERS_BUTTON
+    height_slider = number_circle_box.top - sampling_box.top \
+                    - sampling_box.height - constructed_rectangle.box_padding_ordinate
+
+    left_slider_number_circle = original_drawing_rectangle.width * 1.05
+    top_slider_number_circle = number_circle_box.top + number_circle_box.height \
+                          + (constructed_rectangle.box_padding_ordinate // 2)
+
+    slider_number_circle = Slider(screen, left_slider_number_circle, \
+                             top_slider_number_circle, width_slider, height_slider, \
+                             min = MIN_CIRCLE_SLIDER_VALUE,\
+                             max = MAX_CIRCLE_SLIDER_VALUE, \
+                             step = CIRCLE_SLIDER_STEP, \
+                             handleColour = SLIDER_HANDLE_COLOR, colour = SLIDER_COLOR)
+    return slider_number_circle
--- main.py
+++ main.py
@@ -5,68 +5,17 @@ Fichier main du projet pao "dessine moi un éléphant".
 import sys
 import pygame as pg
 import pygame_widgets
-from pygame_widgets.slider import Slider
 from pygame_widgets.widget import WidgetHandler
 
 from dessiner_des_elephants.ihm.affichage.screen_utils import init_window, clear_screen
 from dessiner_des_elephants.ihm.acquisition.points_acquisition import get_points, \
                                                                       sampling_points
 from dessiner_des_elephants.ihm.affichage.constructed_rectangles import ConstructedRectangles
-from dessiner_des_elephants.ihm.affichage.draw_elephant_utils import SLIDER_COLOR,\
-                                                                     SLIDER_HANDLE_COLOR, \
-                                                                     MIN_CIRCLE, MAX_CIRCLE,\
-                                                                PROPORTION_PARAMETERS_BUTTON
+
+from dessiner_des_elephants.ihm.acquisition.create_slider import create_sampling_slider,\
+                                                                 create_number_circle_slider
+
 from dessiner_des_elephants.logique_metier.point import Point2D
-
-def _create_sampling_slider(screen : pg.Surface,\
-                            constructed_rectangle : ConstructedRectangles,\
-                            points : list[Point2D]):
-
-    original_drawing_rectangle = constructed_rectangle.original_drawing_rectangle
-    sampling_box = constructed_rectangle.sampling_box
-    number_circle_box = constructed_rectangle.number_circle_box
-
-    width_slider = (screen.get_size()[0] - original_drawing_rectangle.width) \
-                   * PROPORTION_PARAMETERS_BUTTON
-    height_slider = number_circle_box.top - sampling_box.top \
-                    - sampling_box.height - constructed_rectangle.box_padding_ordinate
-
-    left_slider_sampling = original_drawing_rectangle.width * 1.05
-    top_slider_sampling = sampling_box.top + sampling_box.height \
-                          + (constructed_rectangle.box_padding_ordinate // 2)
-
-    min_sampling = min(len(points), 10)
-    max_sampling = min(len(points), 2000)
-
-    slider_sampling = Slider(screen, left_slider_sampling, top_slider_sampling, \
-                             width_slider, height_slider, \
-                             min = min_sampling, max = max_sampling, \
-                             step = 5, \
-                             handleColour = SLIDER_HANDLE_COLOR, colour = SLIDER_COLOR)
-    return slider_sampling
-
-def _create_number_circle_slider(screen : pg.Surface,\
-                                 constructed_rectangle : ConstructedRectangles):
-
-    original_drawing_rectangle = constructed_rectangle.original_drawing_rectangle
-    sampling_box = constructed_rectangle.sampling_box
-    number_circle_box = constructed_rectangle.number_circle_box
-
-    width_slider = (screen.get_size()[0] - original_drawing_rectangle.width) \
-                   * PROPORTION_PARAMETERS_BUTTON
-    height_slider = number_circle_box.top - sampling_box.top \
-                    - sampling_box.height - constructed_rectangle.box_padding_ordinate
-
-    left_slider_number_circle = original_drawing_rectangle.width * 1.05
-    top_slider_number_circle = number_circle_box.top + number_circle_box.height \
-                          + (constructed_rectangle.box_padding_ordinate // 2)
-
-    slider_number_circle = Slider(screen, left_slider_number_circle, \
-                             top_slider_number_circle, width_slider, height_slider, \
-                             min = MIN_CIRCLE, max = MAX_CIRCLE, \
-                             step = 1, \
-                             handleColour = SLIDER_HANDLE_COLOR, colour = SLIDER_COLOR)
-    return slider_number_circle
 
 def _get_parameters_from_box(screen : pg.Surface, \
                              constructed_rectangle : ConstructedRectangles,\
@@ -123,11 +72,8 @@ def _get_parameters(screen : pg.Surface, points : list[Point2D],\
     sampling_box = constructed_rectangle.sampling_box
     number_circle_box = constructed_rectangle.number_circle_box
 
-    slider_sampling = _create_sampling_slider(screen, constructed_rectangle, points)
-    slider_number_circle = _create_number_circle_slider(screen, constructed_rectangle)
-
-    sampling_box.slider = slider_sampling
-    number_circle_box.slider = slider_number_circle
+    sampling_box.slider = create_sampling_slider(screen, constructed_rectangle, points)
+    number_circle_box.slider = create_number_circle_slider(screen, constructed_rectangle)
 
     not_done = True
 
@@ -172,8 +118,8 @@ def _get_parameters(screen : pg.Surface, points : list[Point2D],\
         pygame_widgets.update(events)
         pg.display.update()
 
-    WidgetHandler.removeWidget(slider_sampling)
-    WidgetHandler.removeWidget(slider_number_circle)
+    WidgetHandler.removeWidget(sampling_box.slider)
+    WidgetHandler.removeWidget(number_circle_box.slider)
 
     number_circle = number_circle_box.value
     number_points = sampling_box.value
@@ -192,11 +138,11 @@ def _launch_drawing(screen : pg.Surface, constructed_rectangle : ConstructedRect
 
     original_drawing_rectangle.draw_points(points)
 
-    reconstitue_points = sampled_points, number_circle, points = _get_parameters(screen, points,\
+    sampled_points, number_circle, points = _get_parameters(screen, points,\
                                                     constructed_rectangle,\
                                                     number_point, number_circle)
 
-    reconstructed_drawing_rectangle.draw_reconstructed_drawing( \
+    reconstitue_points = reconstructed_drawing_rectangle.draw_reconstructed_drawing( \
         original_drawing_rectangle, sampled_points, number_circle)
 
     return len(sampled_points), number_circle, points, reconstitue_points
@@ -210,7 +156,7 @@ def _show_quit_and_start_box(constructed_rectangle : ConstructedRectangles):
     quit_box.draw()
     quit_box.set_text("Quitter")
 
-def _show_parameters_box(constructed_rectangle : ConstructedRectangles)->None:
+def _show_parameters_box(constructed_rectangle : ConstructedRectangles):
     number_circle_box = constructed_rectangle.number_circle_box
     sampling_box = constructed_rectangle.sampling_box
     redraw_box = constructed_rectangle.redraw_box
@@ -220,7 +166,7 @@ def _show_parameters_box(constructed_rectangle : ConstructedRectangles)->None:
     sampling_box.draw()
     number_circle_box.draw()
 
-def _show_drawing_rectangle(constructed_rectangle : ConstructedRectangles)->None:
+def _show_drawing_rectangle(constructed_rectangle : ConstructedRectangles):
     original_drawing_rectangle = constructed_rectangle.original_drawing_rectangle
     reconstructed_drawing_rectangle = constructed_rectangle.reconstructed_drawing_rectangle
 
